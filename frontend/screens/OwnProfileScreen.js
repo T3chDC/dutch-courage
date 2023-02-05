4a47be1c4843947758c0da706c63edc11b6a45d3
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-02-06 02:57:59 +0600

    Update OwnProfileScreen.js

--- frontend/screens/OwnProfileScreen.js
+++ frontend/screens/OwnProfileScreen.js
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useRoute, useNavigation } from '@react-navigation/native'
 
 const OwnProfileScreen = () => {
+  const navigation = useNavigation()
+
   return (
-    <View>
-      <Text>OwnProfileScreen</Text>
+    <View className='bg-black flex-1 justify-start items-center'>
+      {/* background cutoff image*/}
+      <Image
+        source={require('../assets/projectImages/profileBackgroundCutOff.png')}
+      />
     </View>
   )
 }
