60774741c40bac395b4825f89adbae9fe29a8135
Sakib Patwary <shakibpatwary001@gmail.com>
2023-02-07 20:16:01 +0600

    Update OwnProfileScreen.js

--- frontend/screens/OwnProfileScreen.js
+++ frontend/screens/OwnProfileScreen.js
@@ -5,27 +5,27 @@ import {
   TouchableOpacity,
   Modal,
   TextInput,
-} from 'react-native'
-import { Picker } from '@react-native-picker/picker'
-import * as ImagePicker from 'expo-image-picker'
-import { PlusIcon } from 'react-native-heroicons/solid'
-import React, { useEffect, useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useRoute, useNavigation } from '@react-navigation/native'
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import * as ImagePicker from "expo-image-picker";
+import { PlusIcon } from "react-native-heroicons/solid";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRoute, useNavigation } from "@react-navigation/native";
 
 const OwnProfileScreen = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
-  const [mantra, setMantra] = useState('')
-  const [birthYear, setBirthYear] = useState('')
-  const [gender, setGender] = useState('')
-  const [location, setLocation] = useState('')
-  const [topInterests, setTopInterests] = useState([])
-  const [imageUrl, setImageUrl] = useState('')
-  const [editMode, setEditMode] = useState(false)
-  const [selectedImage, setSelectedImage] = useState(null)
+  const [mantra, setMantra] = useState("");
+  const [birthYear, setBirthYear] = useState("");
+  const [gender, setGender] = useState("");
+  const [location, setLocation] = useState("");
+  const [topInterests, setTopInterests] = useState([]);
+  const [imageUrl, setImageUrl] = useState("");
+  const [editMode, setEditMode] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
   const [isImageChooseModalVisible, setIsImageChooseModalVisible] =
-    useState(false)
+    useState(false);
 
   // Function to pick image from gallery
   const pickImage = async () => {
@@ -34,15 +34,15 @@ const OwnProfileScreen = () => {
       allowsEditing: true,
       aspect: [4, 4],
       quality: 1,
-    })
+    });
 
-    console.log(result)
+    console.log(result);
 
     if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri)
-      setIsImageChooseModalVisible(false)
+      setSelectedImage(result.assets[0].uri);
+      setIsImageChooseModalVisible(false);
     }
-  }
+  };
 
   // Function to take image from camera
   const takeImage = async () => {
@@ -51,74 +51,76 @@ const OwnProfileScreen = () => {
       allowsEditing: true,
       aspect: [4, 4],
       quality: 1,
-    })
+    });
 
-    console.log(result)
+    console.log(result);
 
     if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri)
-      setIsImageChooseModalVisible(false)
+      setSelectedImage(result.assets[0].uri);
+      setIsImageChooseModalVisible(false);
     }
-  }
+  };
 
   // Function to open modal to choose between camera and gallery
   const openModal = () => {
-    setIsImageChooseModalVisible(true)
-  }
+    setIsImageChooseModalVisible(true);
+  };
 
   return (
-    <View className='bg-black flex-1 justify-start items-center relative'>
+    <View className="bg-black flex-1 justify-start items-center relative">
       {/* background cutoff image*/}
       <Image
-        source={require('../assets/projectImages/profileBackgroundCutOff.png')}
-        className='w-[100vw]'
+        source={require("../assets/projectImages/profileBackgroundCutOff.png")}
+        className="w-[100vw] h-[40vh]"
       />
       {/* profile image and image picker */}
-      <View className='mt-[-230] w-64 h-64 rounded-full bg-[#FCFCFE] flex-row justify-center items-center'>
+      <View className="mt-[-230] w-64 h-64 rounded-full bg-[#FCFCFE] flex-row justify-center items-center">
         <TouchableOpacity onPress={() => openModal()}>
           {imageUrl || selectedImage ? (
             <Image
               source={{ uri: selectedImage || imageUrl }}
-              className='w-64 h-64 rounded-full'
+              className="w-64 h-64 rounded-full"
             />
           ) : (
-            <PlusIcon size={40} color={'black'} />
+            <View className="flex justify-center items-center w-56 h-56">
+              <PlusIcon size={40} color={"black"} />
+            </View>
           )}
         </TouchableOpacity>
 
         {/* image picker modal */}
         <Modal
-          animationType='slide'
+          animationType="slide"
           transparent={true}
           visible={isImageChooseModalVisible}
           onRequestClose={() => {
-            setIsImageChooseModalVisible(false)
+            setIsImageChooseModalVisible(false);
           }}
         >
-          <View className='absolute bottom-0 flex-1 justify-center items-center'>
-            <View className='w-[100vw] h-[200] rounded-2xl flex-col justify-between items-center py-5 bg-gray-800'>
+          <View className="absolute bottom-0 flex-1 justify-center items-center">
+            <View className="w-[100vw] h-[200] rounded-2xl flex-col justify-between items-center py-5 bg-gray-800">
               <TouchableOpacity
-                className='bg-[#22A6B3] rounded-full w-40 h-12 flex-row justify-center items-center'
+                className="bg-[#22A6B3] rounded-full w-40 h-12 flex-row justify-center items-center"
                 onPress={() => takeImage()}
               >
-                <Text className='text-white text-base font-semibold'>
+                <Text className="text-white text-base font-semibold">
                   Take Photo
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity
-                className='bg-[#22A6B3] rounded-full w-40 h-12 flex-row justify-center items-center'
+                className="bg-[#22A6B3] rounded-full w-40 h-12 flex-row justify-center items-center"
                 onPress={() => pickImage()}
               >
-                <Text className='text-white text-base font-semibold'>
+                <Text className="text-white text-base font-semibold">
                   Choose from Gallery
                 </Text>
               </TouchableOpacity>
 
               <TouchableOpacity
-                className='bg-[#22A6B3] rounded-full w-40 h-12 flex-row justify-center items-center'
+                className="bg-[#22A6B3] rounded-full w-40 h-12 flex-row justify-center items-center"
                 onPress={() => setIsImageChooseModalVisible(false)}
               >
-                <Text className='text-white text-base font-semibold'>
+                <Text className="text-white text-base font-semibold">
                   Cancel
                 </Text>
               </TouchableOpacity>
@@ -130,23 +132,23 @@ const OwnProfileScreen = () => {
       {editMode ? (
         <>
           {/* mantra */}
-          <View className='mt-5'>
+          <View className="mt-5">
             <TextInput
-              placeholder='Mantra'
-              keyboardType='email-address'
-              className='bg-[#F6F6F6] border border-[#E8E8E8] rounded-md h-12 w-80 px-4 mt-4'
+              placeholder="Mantra"
+              keyboardType="email-address"
+              className="bg-[#F6F6F6] border border-[#E8E8E8] rounded-md h-12 w-80 px-4 mt-4"
               value={mantra}
               onChangeText={(text) => setMantra(text)}
             />
           </View>
 
           {/* birth year */}
-          <View className='mt-2 bg-white h-12 w-28 rounded-md'>
+          <View className="mt-2 bg-white h-12 w-28 rounded-md pl-2 flex justify-center">
             <Picker
               selectedValue={birthYear}
               onValueChange={(itemValue, itemIndex) => setBirthYear(itemValue)}
             >
-              <Picker.Item label='YYYY' value='' color='#898A8D' />
+              <Picker.Item label="YYYY" value="" color="#898A8D" />
               {[...Array(101).keys()]
                 .map((i) => 1950 + i)
                 .map((year) => (
@@ -156,22 +158,22 @@ const OwnProfileScreen = () => {
           </View>
 
           {/* Gender */}
-          <View className='mt-2 bg-white h-12 w-40 rounded-md'>
+          <View className="mt-2 bg-white h-12 w-40 rounded-full pl-12 flex justify-center">
             <Picker
               selectedValue={gender}
               onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
             >
-              <Picker.Item label='Gender' value='' color='#898A8D' />
-              <Picker.Item label='Male' value='male' />
-              <Picker.Item label='Female' value='female' />
-              <Picker.Item label='Others' value='others' />
+              <Picker.Item label="Gender" value="" color="#898A8D" />
+              <Picker.Item label="Male" value="male" />
+              <Picker.Item label="Female" value="female" />
+              <Picker.Item label="Others" value="others" />
             </Picker>
           </View>
 
           {/* Location */}
           <View>
-            <TouchableOpacity className='bg-[#F6F6F6] border border-[#E8E8E8] flex rounded-full h-12 w-40 px-4 mt-4 flex-row justify-center items-center'>
-              <Text className='text-[#898A8D] text-base font-medium'>
+            <TouchableOpacity className="bg-[#F6F6F6] border border-[#E8E8E8] flex rounded-full h-12 w-40 px-4 mt-4 flex-row justify-center items-center">
+              <Text className="text-[#898A8D] text-base font-medium">
                 Location
               </Text>
             </TouchableOpacity>
@@ -179,8 +181,8 @@ const OwnProfileScreen = () => {
 
           {/* Top Interests */}
           <View>
-            <TouchableOpacity className='bg-[#F6F6F6] border border-[#E8E8E8] flex rounded-full h-12 w-80 px-4 mt-4 flex-row justify-center items-center'>
-              <Text className='text-[#898A8D] text-base font-medium'>
+            <TouchableOpacity className="bg-[#F6F6F6] border border-[#E8E8E8] flex rounded-full h-12 w-80 px-4 mt-4 flex-row justify-center items-center">
+              <Text className="text-[#898A8D] text-base font-medium">
                 3 Top Interests
               </Text>
             </TouchableOpacity>
@@ -190,28 +192,28 @@ const OwnProfileScreen = () => {
 
       {/* edit profile button */}
       {!editMode ? (
-        <View className='mt-4'>
+        <View className="mt-4">
           <TouchableOpacity
-            className='bg-[#22A6B3] rounded-full h-12 w-40 flex-row justify-center items-center'
+            className="bg-[#22A6B3] rounded-full h-12 w-40 flex-row justify-center items-center"
             onPress={() => setEditMode(true)}
           >
-            <Text className='text-white text-base font-semibold'>
+            <Text className="text-white text-base font-semibold">
               Edit Profile
             </Text>
           </TouchableOpacity>
         </View>
       ) : (
-        <View className='mt-4'>
+        <View className="mt-4">
           <TouchableOpacity
-            className='bg-[#22A6B3] rounded-full w-40 h-12 flex-row justify-center items-center'
+            className="bg-[#22A6B3] rounded-full w-40 h-12 flex-row justify-center items-center"
             onPress={() => setEditMode(false)}
           >
-            <Text className='text-white text-base font-semibold'>Save</Text>
+            <Text className="text-white text-base font-semibold">Save</Text>
           </TouchableOpacity>
         </View>
       )}
     </View>
-  )
-}
+  );
+};
 
-export default OwnProfileScreen
+export default OwnProfileScreen;
