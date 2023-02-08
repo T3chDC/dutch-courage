8a48db434258f53a6749e3b0431b5fa65b504de3
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-02-08 15:48:53 +0600

    Update app.js

--- backend/config/app.js
+++ backend/config/app.js
@@ -8,7 +8,10 @@ dotenv.config() //load the .env file
 
 import express from 'express' //import express from express
 import cors from 'cors' //import cors for CROSS-ORIGIN-RESOURCE-SHARING
+
 import globalErrorHandler from '../api/v1/utils/errorController.js' //import global error handler
+import AppError from '../api/v1/utils/appError.js' //import appError for error report creation
+
 import v1Router from '../api/v1/common/v1Routes.js' //import v1Router from v1Routes.js
 
 const app = express() //create an instance of express
@@ -21,6 +24,10 @@ app.use('/api/v1', v1Router) //use v1Router for /api/v1
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'API is running...', app: 'dutch-courage' })
+})
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
 })
 
 app.use(globalErrorHandler) //use global error handler
