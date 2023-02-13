47b17464b0d51e5392e6edf75060006b2631ddc5
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-02-13 18:53:46 +0600

    add features and slices

---
+++ frontend/features/auth/authService.js
@@ -0,0 +1,30 @@
+// This file is responsible for making the API calls to the backend
+import axios from 'axios'
+import { BACKEND_URL } from '@env'
+
+const API_URL = BACKEND_URL + '/api/v1/users'
+
+//signup user locally
+const signupLocal = async (userData) => {
+  const response = await axios.post(API_URL + '/signup/local', userData)
+  if (response.data.status === 'success') {
+    //store in async storage of device
+  }
+  return response.data.data
+}
+
+//signin user locally
+const signinLocal = async (userData) => {
+  const response = await axios.post(API_URL + '/signin/local', userData)
+  if (response.data.status === 'success') {
+    //store in async storage of device
+  }
+  return response.data.data
+}
+
+const authService = {
+  signupLocal,
+  signinLocal,
+}
+
+export default authService
---
+++ frontend/features/auth/authSlice.js
@@ -0,0 +1,5 @@
+// This file is responsible for handling the state of the authentication process
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
+
+
