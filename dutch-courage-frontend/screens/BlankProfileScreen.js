723a7719f5cfbd1f54b717834876b0c7daca7fd9
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-04-05 02:48:19 +0600

    changed blank profile functionalities

--- dutch-courage-frontend/screens/BlankProfileScreen.js
+++ dutch-courage-frontend/screens/BlankProfileScreen.js
@@ -23,13 +23,15 @@ const BlankProfileScreen = () => {
   const dispatch = useDispatch()
 
   // Local State variables
+  const [userName, setUserName] = useState('')
+  const [userNameCount, setUserNameCount] = useState(0)
   const [mantra, setMantra] = useState('')
+  const [mantraCount, setMantraCount] = useState(0)
   const [birthYear, setBirthYear] = useState('')
   const [gender, setGender] = useState('')
   const [location, setLocation] = useState('')
   const [topInterests, setTopInterests] = useState([])
   const [imageUrl, setImageUrl] = useState('')
-  const [editMode, setEditMode] = useState(false)
   const [selectedImage, setSelectedImage] = useState(null)
 
   // Modal State variables
@@ -42,10 +44,6 @@ const BlankProfileScreen = () => {
 
   const {
     meUser,
-    isMeGetError,
-    isMeGetSuccess,
-    isMeGetLoading,
-    meGetErrorMessage,
     isMeUpdateError,
     isMeUpdateSuccess,
     isMeUpdateLoading,
@@ -57,25 +55,6 @@ const BlankProfileScreen = () => {
       navigation.navigate('Login')
     }
   }, [userInfo, navigation])
-
-  useEffect(() => {
-    if (isMeGetError) {
-      Toast.show({
-        type: 'error',
-        text1: meGetErrorMessage,
-        visibilityTime: 3000,
-      })
-    } else if (isMeGetSuccess) {
-      setMantra(meUser.mantra)
-      setBirthYear(meUser.birthYear)
-      setGender(meUser.gender)
-      setLocation(meUser.location)
-      setTopInterests(meUser.topInterests)
-      setImageUrl(meUser.imageUrl)
-    } else {
-      dispatch(getMeUser())
-    }
-  }, [isMeGetError, isMeGetSuccess, meGetErrorMessage, dispatch, meUser])
 
   useEffect(() => {
     if (isMeUpdateError) {
@@ -90,14 +69,12 @@ const BlankProfileScreen = () => {
         text1: 'Profile Updated Successfully',
         visibilityTime: 3000,
       })
-      setEditMode(false)
       dispatch(getMeUser())
     }
   }, [isMeUpdateError, isMeUpdateSuccess, meUpdateErrorMessage, dispatch])
 
   useEffect(() => {
     return () => {
-      dispatch(resetMeUser())
       dispatch(resetMeUpdateUser())
     }
   }, [dispatch])
@@ -183,11 +160,8 @@ const BlankProfileScreen = () => {
         className='w-[100vw] h-[40vh]'
       />
       {/* profile image and image picker */}
-      <View className='mt-[-230] w-64 h-64 rounded-full bg-[#FCFCFE] flex-row justify-center items-center'>
-        <TouchableOpacity
-          onPress={() => setIsImageChooseModalVisible(true)}
-          disabled={!editMode}
-        >
+      <View className='mt-[-240] w-64 h-64 rounded-full bg-[#FCFCFE] flex-row justify-center items-center'>
+        <TouchableOpacity onPress={() => setIsImageChooseModalVisible(true)}>
           {imageUrl || selectedImage ? (
             <Image
               source={{ uri: selectedImage || imageUrl }}
@@ -207,207 +181,161 @@ const BlankProfileScreen = () => {
           setSelectedImage={setSelectedImage}
         />
       </View>
-      {!isMeGetLoading && !isMeUpdateLoading && (
+      {!isMeUpdateLoading && (
         <>
-          {editMode ? (
-            <>
-            {/* name */}
-              <View className='mt-2'>
-                <TextInput
-                  placeholder='Username'
-                  keyboardType='email-address'
-                  className='bg-[#F6F6F6] border border-[#E8E8E8] rounded-md h-12 w-80 px-4 mt-4'
-                  value='username here'
-                  editable={false}
-                  // onChangeText={(text) => setMantra(text)}
-                />
-              </View>
+          {/* username character counter and limiter */}
+          <View className='flex-row justify-center items-center'>
+            <Text className='text-[#898A8D] text-xs mt-2 ml-64'>
+              {userNameCount}/35
+            </Text>
+          </View>
 
-              {/* mantra */}
-              <View>
-                <TextInput
-                  placeholder='Mantra'
-                  keyboardType='email-address'
-                  className='bg-[#F6F6F6] border border-[#E8E8E8] rounded-md h-12 w-80 px-4 mt-4'
-                  value={mantra}
-                  onChangeText={(text) => setMantra(text)}
-                />
-              </View>
+          {/* UserName */}
+          <View>
+            <TextInput
+              placeholder='Username'
+              keyboardType='default'
+              className='bg-[#F6F6F6] border border-[#E8E8E8] rounded-md h-10 w-80 px-4'
+              value={userName}
+              onChangeText={(text) => {
+                if (text.length <= 35) {
+                  setUserName(text)
+                  setUserNameCount(text.length)
+                }
+              }}
+            />
+          </View>
 
-              {/* birth year */}
-              <View className='mt-2 bg-white h-12 w-36 rounded-md flex justify-center'>
-                <Picker
-                  selectedValue={birthYear?.toString()}
-                  onValueChange={(itemValue, itemIndex) =>
-                    setBirthYear(itemValue * 1)
-                  }
-                >
-                  <Picker.Item
-                    label='YYYY'
-                    enabled={false}
-                    value=''
-                    color='#898A8D'
-                  />
-                  {[...Array(71).keys()]
-                    .map((i) => 2020 - i)
-                    .map((year) => (
-                      <Picker.Item
-                        key={year}
-                        label={`${year}`}
-                        value={`${year}`}
-                      />
-                    ))}
-                </Picker>
-              </View>
+          {/* mantra character counter and limiter */}
+          <View className='flex-row justify-center items-center'>
+            <Text className='text-[#898A8D] text-xs ml-64'>
+              {mantraCount}/40
+            </Text>
+          </View>
 
-              {/* Gender */}
-              <View className='mt-2 bg-white h-12 w-40 rounded-full flex justify-center'>
-                <Picker
-                  selectedValue={gender}
-                  onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
-                >
-                  <Picker.Item
-                    label='Gender'
-                    enabled={false}
-                    value=''
-                    color='#898A8D'
-                  />
-                  <Picker.Item label='Male' value='male' />
-                  <Picker.Item label='Female' value='female' />
-                  <Picker.Item label='Others' value='others' />
-                </Picker>
-              </View>
+          {/* mantra */}
+          <View>
+            <TextInput
+              placeholder='Mantra'
+              keyboardType='default'
+              className='bg-[#F6F6F6] border border-[#E8E8E8] rounded-md h-10 w-80 px-4'
+              value={mantra}
+              onChangeText={(text) => {
+                if (text.length <= 40) {
+                  setMantra(text)
+                  setMantraCount(text.length)
+                }
+              }}
+            />
+          </View>
 
-              {/* Location */}
-              <View>
-                <TouchableOpacity
-                  className='bg-[#F6F6F6] border border-[#E8E8E8] flex rounded-full h-12 w-40 px-4 mt-4 flex-row justify-center items-center'
-                  onPress={() => setIsLocationModalVisible(true)}
-                >
-                  <Text
-                    className={
-                      location
-                        ? `text-black text-base font-medium`
-                        : `text-[#898A8D] text-base font-medium`
-                    }
-                  >
-                    {location ? location : 'Location'}
-                  </Text>
-                </TouchableOpacity>
-              </View>
-
-              {/* Location picker modal */}
-              <LocationPickerModal
-                isLocationModalVisible={isLocationModalVisible}
-                setIsLocationModalVisible={setIsLocationModalVisible}
-                setLocation={setLocation}
-              />
-
-              {/* Top Interests */}
-              <View>
-                <TouchableOpacity
-                  className='bg-[#F6F6F6] border border-[#E8E8E8] flex rounded-full h-12 w-80 px-4 mt-4 flex-row justify-center items-center'
-                  onPress={() => setIsInterestModalVisible(true)}
-                >
-                  <Text
-                    className={
-                      topInterests && topInterests.length > 0
-                        ? `text-black text-base font-medium`
-                        : 'text-[#898A8D] text-base font-medium'
-                    }
-                  >
-                    {topInterests && topInterests.length === 0
-                      ? '3 Top Interests'
-                      : `${topInterests[0]}, ${topInterests[1]}, ${topInterests[2]}`}
-                  </Text>
-                </TouchableOpacity>
-              </View>
-
-              {/* Interst picker modal */}
-              <InterestPickerModal
-                isInterestModalVisible={isInterestModalVisible}
-                setIsInterestModalVisible={setIsInterestModalVisible}
-                setTopInterests={setTopInterests}
-              />
-            </>
-          ) : (
-            <>
-              {/* mantra */}
-              <View className='mt-5'>
-                <Text className='text-white text-base font-medium'>
-                  {mantra}
-                </Text>
-              </View>
-
-              {/* birth year */}
-              <View className='mt-2'>
-                <Text className='text-white text-base font-medium'>
-                  {birthYear}
-                </Text>
-              </View>
-
-              {/* Gender */}
-              <View className='mt-2'>
-                <Text className='text-white text-base font-medium'>
-                  {gender}
-                </Text>
-              </View>
-
-              {/* Location */}
-              <View className='mt-2'>
-                <Text className='text-white text-base font-medium'>
-                  {location}
-                </Text>
-              </View>
-
-              {/* Top Interests */}
-              <View className='mt-2'>
-                <Text className='text-white text-base font-medium'>
-                  {topInterests &&
-                    topInterests.length > 0 &&
-                    `${topInterests[0]}, ${topInterests[1]}, ${topInterests[2]}`}
-                </Text>
-              </View>
-            </>
-          )}
-        </>
-      )}
-
-      {/* edit profile button */}
-      {!editMode ? (
-        <>
-          <View className='mt-4'>
-            <TouchableOpacity
-              className='bg-[#22A6B3] rounded-full h-12 w-40 flex-row justify-center items-center'
-              onPress={() => setEditMode(true)}
+          {/* birth year */}
+          <View className='mt-2 bg-white h-10 w-36 rounded-md flex justify-center'>
+            <Picker
+              selectedValue={birthYear?.toString()}
+              onValueChange={(itemValue, itemIndex) =>
+                setBirthYear(itemValue * 1)
+              }
             >
-              <Text className='text-white text-base font-semibold'>
-                Edit Profile
+              <Picker.Item
+                label='YYYY'
+                enabled={false}
+                value=''
+                color='#898A8D'
+              />
+              {[...Array(71).keys()]
+                .map((i) => 2020 - i)
+                .map((year) => (
+                  <Picker.Item key={year} label={`${year}`} value={`${year}`} />
+                ))}
+            </Picker>
+          </View>
+
+          {/* Gender */}
+          <View className='mt-2 bg-white h-10 w-40 rounded-full flex justify-center'>
+            <Picker
+              selectedValue={gender}
+              onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
+            >
+              <Picker.Item
+                label='Gender'
+                enabled={false}
+                value=''
+                color='#898A8D'
+              />
+              <Picker.Item label='Male' value='male' />
+              <Picker.Item label='Female' value='female' />
+              <Picker.Item label='Others' value='others' />
+            </Picker>
+          </View>
+
+          {/* Location */}
+          <View>
+            <TouchableOpacity
+              className='bg-[#F6F6F6] border border-[#E8E8E8] flex rounded-full h-10 w-40 px-4 mt-4 flex-row justify-center items-center'
+              onPress={() => setIsLocationModalVisible(true)}
+            >
+              <Text
+                className={
+                  location
+                    ? `text-black text-base font-medium`
+                    : `text-[#898A8D] text-base font-medium`
+                }
+              >
+                {location ? location : 'Location'}
               </Text>
             </TouchableOpacity>
           </View>
-          {isMeGetLoading && (
-            <View className='mt-8'>
-              <Progress.CircleSnail color='#22A6B3' size={60} />
-            </View>
-          )}
-        </>
-      ) : (
-        <>
-          <View className='mt-4'>
+
+          {/* Location picker modal */}
+          <LocationPickerModal
+            isLocationModalVisible={isLocationModalVisible}
+            setIsLocationModalVisible={setIsLocationModalVisible}
+            setLocation={setLocation}
+          />
+
+          {/* Top Interests */}
+          <View>
             <TouchableOpacity
-              className='bg-[#22A6B3] rounded-full w-40 h-12 flex-row justify-center items-center'
-              onPress={updateUserHandler}
+              className='bg-[#F6F6F6] border border-[#E8E8E8] flex rounded-full h-10 w-80 px-4 mt-4 flex-row justify-center items-center'
+              onPress={() => setIsInterestModalVisible(true)}
             >
-              <Text className='text-white text-base font-semibold'>Save</Text>
+              <Text
+                className={
+                  topInterests && topInterests.length > 0
+                    ? `text-black text-base font-medium`
+                    : 'text-[#898A8D] text-base font-medium'
+                }
+              >
+                {topInterests && topInterests.length === 0
+                  ? '3 Top Interests'
+                  : `${topInterests[0]}, ${topInterests[1]}, ${topInterests[2]}`}
+              </Text>
             </TouchableOpacity>
           </View>
-          {isMeUpdateLoading && (
-            <View className='mt-8'>
-              <Progress.CircleSnail color='#22A6B3' size={60} />
-            </View>
-          )}
+
+          {/* Interst picker modal */}
+          <InterestPickerModal
+            isInterestModalVisible={isInterestModalVisible}
+            setIsInterestModalVisible={setIsInterestModalVisible}
+            setTopInterests={setTopInterests}
+          />
         </>
+      )}
+
+      {/* save button */}
+      <View className='mt-4'>
+        <TouchableOpacity
+          className='bg-[#22A6B3] rounded-full w-40 h-10 flex-row justify-center items-center'
+          onPress={updateUserHandler}
+        >
+          <Text className='text-white text-base font-semibold'>Save</Text>
+        </TouchableOpacity>
+      </View>
+      {isMeUpdateLoading && (
+        <View className='mt-8'>
+          <Progress.CircleSnail color='#22A6B3' size={60} />
+        </View>
       )}
     </View>
   )
