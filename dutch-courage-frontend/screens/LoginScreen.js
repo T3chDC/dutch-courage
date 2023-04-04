06aa7e4343efbfbdb996cafd3d28e6828176c89b
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-04-04 13:33:20 +0600

    implemented back action in login page

--- dutch-courage-frontend/screens/LoginScreen.js
+++ dutch-courage-frontend/screens/LoginScreen.js
@@ -1,4 +1,12 @@
-import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+  BackHandler,
+} from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
@@ -51,6 +59,31 @@ const LoginScreen = () => {
     isSignInError,
     signInErrorMessage,
   } = useSelector((state) => state.auth)
+
+  //Exit App on Back Press
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert(
+        'Hold on!',
+        'Are you sure you want to exit the app?',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => null,
+            style: 'cancel',
+          },
+          { text: 'YES', onPress: () => BackHandler.exitApp() },
+        ],
+        { cancelable: false }
+      )
+      return true
+    }
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction
+    )
+    return () => backHandler.remove()
+  }, [])
 
   useEffect(() => {
     if (userInfo && userInfo.newUser) {
