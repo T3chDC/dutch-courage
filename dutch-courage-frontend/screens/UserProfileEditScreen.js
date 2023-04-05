b0ab80442f8b75033e72666ff3521382a1984b75
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-04-06 00:40:09 +0600

    Update UserProfileEditScreen.js

--- dutch-courage-frontend/screens/UserProfileEditScreen.js
+++ dutch-courage-frontend/screens/UserProfileEditScreen.js
@@ -7,8 +7,37 @@ import {
   BackHandler,
 } from 'react-native'
 import React, { useEffect, useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const UserProfileEditScreen = () => {
+  // Navigation hook
+  const navigation = useNavigation()
+
+  // Inform user that the changes made will be lost when back is pressed
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert(
+        'Hold on!',
+        'Are you sure you want to exit the edit profile screen? All changes will be lost.',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => null,
+            style: 'cancel',
+          },
+          { text: 'YES', onPress: () => navigation.goBack() },
+        ],
+        { cancelable: false }
+      )
+      return true
+    }
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction
+    )
+    return () => backHandler.remove()
+  }, [])
+
   return (
     <View className='bg-black flex-1 justify-start items-center relative'>
       {/* background cutoff image*/}
