33143742a9087f596a472bdbea81706888ecc746
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-04-27 14:20:44 +0600

    Update UserProfileEditScreen.js

--- dutch-courage-frontend/screens/UserProfileEditScreen.js
+++ dutch-courage-frontend/screens/UserProfileEditScreen.js
@@ -13,30 +13,30 @@ const UserProfileEditScreen = () => {
   // Navigation hook
   const navigation = useNavigation()
 
-  // Inform user that the changes made will be lost when back is pressed
-  // useEffect(() => {
-  //   const backAction = () => {
-  //     Alert.alert(
-  //       'Hold on!',
-  //       'Are you sure you want to exit the edit profile screen? All changes will be lost.',
-  //       [
-  //         {
-  //           text: 'Cancel',
-  //           onPress: () => null,
-  //           style: 'cancel',
-  //         },
-  //         { text: 'YES', onPress: () => navigation.goBack() },
-  //       ],
-  //       { cancelable: false }
-  //     )
-  //     return true
-  //   }
-  //   const backHandler = BackHandler.addEventListener(
-  //     'hardwareBackPress',
-  //     backAction
-  //   )
-  //   return () => backHandler.remove()
-  // }, [])
+  //Inform user that the changes made will be lost when back is pressed
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert(
+        'Hold on!',
+        'The changes you made will be automatically saved when you leave this screen. Are you sure you want to exit the profile editing screen?',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => null,
+            style: 'cancel',
+          },
+          { text: 'YES', onPress: () => navigation.goBack() },
+        ],
+        { cancelable: false }
+      )
+      return true
+    }
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction
+    )
+    return () => backHandler.remove()
+  }, [])
 
   return (
     <View className='bg-black flex-1 justify-start items-center relative'>
