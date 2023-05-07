d19707cf2b1c1290e2a5bd5e5be35324360bb2c6
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-05-07 13:39:30 +0600

    Update userController.js

--- dutch-courage-backend/api/v1/user/userController.js
+++ dutch-courage-backend/api/v1/user/userController.js
@@ -66,6 +66,10 @@ export const updateMe = catchAsync(async (req, res, next) => {
     runValidators: true,
   })
 
+  if (!updatedUser) {
+    return next(new AppError('No user found with that ID', 404))
+  }
+
   res.status(200).json({
     status: 'success',
     data: updatedUser,
