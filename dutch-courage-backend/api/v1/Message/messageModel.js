7bdc65af6643646d2603877f571ded268196f508
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-05-25 12:27:05 +0600

    Create messageModel.js

---
+++ dutch-courage-backend/api/v1/Message/messageModel.js
@@ -0,0 +1,57 @@
+/* This file contains the data modelling and data tier fuinctionalities for messages */
+
+import mongoose from 'mongoose' //import mongoose
+
+//create a message schema
+
+const messageSchema = new mongoose.Schema(
+  {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'a message must belong to a conversation'],
+      ref: 'Conversation',
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'a message must have a sender'],
+      ref: 'User',
+    },
+
+    messageType: {
+      type: String,
+      enum: ['text', 'image'],
+      default: 'text',
+    },
+
+    message: {
+      type: String,
+      required: [
+        function () {
+          return this.messageType === 'text'
+        },
+        ,
+        'a message must have a message body',
+      ],
+    },
+
+    imageUrl: {
+      type: String,
+      required: [
+        function () {
+          return this.messageType === 'image'
+        },
+        ,
+        'a message must have an image url',
+      ],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// Create a message model
+
+const Message = mongoose.model('Message', messageSchema)
+
+export default Message
