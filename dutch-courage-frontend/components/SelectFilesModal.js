b069766536e3f97abaaab9eb08b6b10332c2e94a
Diponkor Chandra Shil <66906170+diponkorcs@users.noreply.github.com>
2023-06-17 14:07:36 +0600

    select files modal updated

--- dutch-courage-frontend/components/SelectFilesModal.js
+++ dutch-courage-frontend/components/SelectFilesModal.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import Modal from "react-native-modal";
 import { View, Text, TouchableOpacity } from "react-native";
 
-const SelectFilesModal = ({ 
-    isSelectFileModalVisible,
-    setIsSelectFileModalVisible,
- }) => {
+const SelectFilesModal = ({
+  isSelectFileModalVisible,
+  setIsSelectFileModalVisible,
+}) => {
   return (
     <Modal
       animationIn={"slideInUp"}
@@ -15,28 +15,25 @@ const SelectFilesModal = ({
       onRequestClose={() => {
         setIsSelectFileModalVisible(false);
       }}
-
     >
       <View className="flex-1 justify-end items-center">
         <View className="w-[100vw] h-[220] rounded flex-col justify-between items-center py-5 bg-white">
-            <TouchableOpacity
-                className="bg-[#22A6B3] rounded-md w-80 h-12 flex-row justify-center items-center"
-            >
-                <Text className="text-white font-bold">Take a picture</Text>
-            </TouchableOpacity>
+          <TouchableOpacity className="bg-[#22A6B3] rounded-md w-80 h-12 flex-row justify-center items-center">
+            <Text className="text-white font-bold">Take a picture</Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-                className="bg-[#22A6B3] rounded-md w-80 h-12 flex-row justify-center items-center"
-            >
-                <Text className="text-white font-bold">Send from gallery</Text>
-            </TouchableOpacity>
+          <TouchableOpacity className="bg-[#22A6B3] rounded-md w-80 h-12 flex-row justify-center items-center">
+            <Text className="text-white font-bold">Send from gallery</Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-                className="border-2 border-[#22A6B3] rounded-md w-80 h-12 flex-row justify-center items-center"
+          <TouchableOpacity className="border-2 border-[#22A6B3] rounded-md w-80 h-12 flex-row justify-center items-center">
+            <Text
+              className="text-[#22A6B3] font-bold"
+              onPress={() => setIsSelectFileModalVisible(false)}
             >
-                <Text className="text-[#22A6B3] font-bold">Cancel</Text>
-            </TouchableOpacity>
-            
+              Cancel
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </Modal>
