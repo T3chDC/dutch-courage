eac34298a01364cb79440a496bd8de7722a07070
Diponkor Chandra Shil <66906170+diponkorcs@users.noreply.github.com>
2023-07-24 18:43:39 +0600

    Locationfinderscreen updated

--- dutch-courage-frontend/screens/LocationFinderScreen.js
+++ dutch-courage-frontend/screens/LocationFinderScreen.js
@@ -5,8 +5,11 @@ import {
   Image,
   TouchableOpacity,
   BackHandler,
+  KeyboardAvoidingView,
+  Keyboard,
+  ScrollView,
 } from "react-native";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { MagnifyingGlassIcon } from "react-native-heroicons/solid";
 import { useDispatch, useSelector } from "react-redux";
@@ -31,6 +34,8 @@ import MapView, { Marker } from "react-native-maps";
 const LocationFinderScreen = () => {
   const navigation = useNavigation();
   const dispatch = useDispatch();
+
+  const scrollViewRef = useRef(null);
 
   const { userInfo } = useSelector((state) => state.auth);
   const {
@@ -122,6 +127,18 @@ const LocationFinderScreen = () => {
     }
   }, [isMeGetError, isMeGetSuccess, meGetErrorMessage, dispatch]);
 
+  // Scroll to bottom of the screen when keyboard is open
+  useEffect(() => {
+    Keyboard.addListener("keyboardDidShow", () =>
+      scrollViewRef.current.scrollToEnd({ animated: true })
+    );
+
+    // cleanup function
+    return () => {
+      Keyboard.removeAllListeners("keyboardDidShow");
+    };
+  }, []);
+
   return (
     <View className="bg-black flex-1 justify-start items-center relative">
       <TouchableOpacity
@@ -134,7 +151,7 @@ const LocationFinderScreen = () => {
       </TouchableOpacity>
 
       <View className="top-[8vh] justify-evenly items-center ">
-        <View className="flex flex-row">
+        {/* <View className="flex flex-row">
           <MapView
             initialRegion={{
               latitude: 23.7941139,
@@ -151,45 +168,83 @@ const LocationFinderScreen = () => {
               coordinate={{ latitude: 23.7941139, longitude: 90.4038988 }}
             ></Marker>
           </MapView>
-        </View>
+        </View> */}
 
-        <View className="mt-5">
-          <Text className="text-white text-sm font-bold left-[10vw]">
-            Search Your Location
-          </Text>
+        <KeyboardAvoidingView
+          behavior={"height"}
+          className="flex-1 flex-col justify-end items-center w-100 h-[650] "
+        >
+          <ScrollView
+            // className='flex flex-col justify-end items-center w-80'
 
-          {/* Search Location Text Field */}
-          <View className="left-[10vw]">
-            <TextInput
-              placeholder="&nbsp; Find Me"
-              placeholderTextColor="#808080"
-              keyboardType="default"
-              className="text-white text-base w-[320] h-10 flex-row mt-5 border-2 rounded-xl border-[#22A6B3] text-sm px-10"
-            />
-            <TouchableOpacity className="bottom-8 left-2 w-[20]">
-              <MagnifyingGlassIcon color="#22A6B3" />
-            </TouchableOpacity>
-          </View>
-
-          {/* Locations */}
-          <View className="">
-            <View className="left-[15vw]">
-              <Text className="text-white text-base font-bold">Jax Bar</Text>
-              <Text className="text-white text-sm">
-                5-7 Brunswick Rd, Gloucester
-              </Text>
+            ref={scrollViewRef}
+            onContentSizeChange={() =>
+              scrollViewRef.current.scrollToEnd({ animated: true })
+            }
+          >
+            <View className="flex flex-row">
+              <MapView
+                initialRegion={{
+                  latitude: 23.7941139,
+                  longitude: 90.4038988,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }}
+                style={{
+                  width: 400,
+                  height: 450,
+                }}
+              >
+                <Marker
+                  coordinate={{ latitude: 23.7941139, longitude: 90.4038988 }}
+                ></Marker>
+              </MapView>
             </View>
-            <View className="flex flex-row left-12 mt-[15] h-[1] w-[400] bg-[#22A6B3]"></View>
 
-            <View className="left-[15vw] mt-5">
-              <Text className="text-white text-base font-bold">Jax Bar</Text>
-              <Text className="text-white text-sm">
-                5-7 Brunswick Rd, Gloucester
+            
+            <View className="mt-5">
+              <Text className="text-white text-sm font-bold left-[10vw]">
+                Search Your Location
               </Text>
+
+              {/* Search Location Text Field */}
+              <View className="left-[10vw]">
+                <TextInput
+                  placeholder="&nbsp; Find Me"
+                  placeholderTextColor="#808080"
+                  keyboardType="default"
+                  className="text-white text-base w-[320] h-10 flex-row mt-5 border-2 rounded-xl border-[#22A6B3] text-sm px-10"
+                />
+                <TouchableOpacity className="bottom-8 left-2 w-[20]">
+                  <MagnifyingGlassIcon color="#22A6B3" />
+                </TouchableOpacity>
+              </View>
+
+              {/* Locations */}
+              <View className="">
+                <View className="left-[15vw]">
+                  <Text className="text-white text-base font-bold">
+                    Jax Bar
+                  </Text>
+                  <Text className="text-white text-sm">
+                    5-7 Brunswick Rd, Gloucester
+                  </Text>
+                </View>
+                <View className="flex flex-row left-12 mt-[15] h-[1] w-[400] bg-[#22A6B3]"></View>
+
+                <View className="left-[15vw] mt-5">
+                  <Text className="text-white text-base font-bold">
+                    Jax Bar
+                  </Text>
+                  <Text className="text-white text-sm">
+                    5-7 Brunswick Rd, Gloucester
+                  </Text>
+                </View>
+                <View className="flex flex-row left-12 mt-[15] h-[1] w-[400] bg-[#22A6B3]"></View>
+              </View>
             </View>
-            <View className="flex flex-row left-12 mt-[15] h-[1] w-[400] bg-[#22A6B3]"></View>
-          </View>
-        </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
       </View>
     </View>
   );
