9fab8da7db532bdaefbe5efc405ab3b98d062ee9
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2024-05-31 00:19:08 +0600

    Update App.js

--- dutch-courage-frontend/App.js
+++ dutch-courage-frontend/App.js
@@ -1,8 +1,12 @@
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 import NavigationHandler from './navigation/NavigationHandler'
+import socket from './utils/socketInit'
 
 export default function App() {
+  // Initialize socket connection
+  socket.connect()
+
   return (
     <>
       <Provider store={store}>
