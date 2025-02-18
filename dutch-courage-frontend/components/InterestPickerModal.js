9fe1f5b694620f1bdeeb24a03b06be0c2c61e6b4
Tanvir Reza Utso <diponkorchandrashil678@gmail.com>
2025-02-18 16:00:05 +0600

    Interest picking limit has been set upto 3

--- dutch-courage-frontend/components/InterestPickerModal.js
+++ dutch-courage-frontend/components/InterestPickerModal.js
@@ -1,16 +1,16 @@
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
-import Modal from 'react-native-modal'
-import React, { useState } from 'react'
-import interests from '../assets/staticData/interests'
-import Toast from 'react-native-toast-message'
-import ToastConfig from '../utils/toastConfig'
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import Modal from "react-native-modal";
+import React, { useState } from "react";
+import interests from "../assets/staticData/interests";
+import Toast from "react-native-toast-message";
+import ToastConfig from "../utils/toastConfig";
 
 const InterestPickerModal = ({
   isInterestModalVisible,
   setIsInterestModalVisible,
   setTopInterests,
 }) => {
-  const [selectedInterests, setSelectedInterests] = useState([])
+  const [selectedInterests, setSelectedInterests] = useState([]);
 
   //Function to handle interest selection
   const handleInterestSelection = (interest) => {
@@ -19,77 +19,89 @@ const InterestPickerModal = ({
         selectedInterests.filter(
           (selectedInterest) => selectedInterest !== interest
         )
-      )
+      );
     } else {
-      setSelectedInterests([...selectedInterests, interest])
+      setSelectedInterests([...selectedInterests, interest]);
     }
-  }
+  };
 
   //Function to handle interest submit
   const handleInterestSubmit = () => {
     if (selectedInterests.length < 3) {
       Toast.show({
-        type: 'error',
-        text1: 'Please select at least 3 interests',
+        type: "error",
+        text1: "Please select at least 3 interests",
         visibilityTime: 3000,
         autoHide: true,
-      })
+      });
     } else {
-      setTopInterests(selectedInterests)
-      setIsInterestModalVisible(false)
+      setTopInterests(selectedInterests);
+      setIsInterestModalVisible(false);
     }
-  }
+  };
 
   return (
     <Modal
-      animationIn={'slideInUp'}
-      animationOut={'slideOutDown'}
+      animationIn={"slideInUp"}
+      animationOut={"slideOutDown"}
       isVisible={isInterestModalVisible}
       onBackdropPress={() => setIsInterestModalVisible(false)}
       onRequestClose={() => {
-        setIsInterestModalVisible(false)
+        setIsInterestModalVisible(false);
       }}
       avoidKeyboard={true}
     >
-      <View className='flex-1 justify-center items-center'>
-        <View className=' absolute top-4 w-[100vw] h-[80vh] rounded-2xl justify-start items-center py-5'>
-          <Text className='text-white text-base font-semibold'>
+      <View className="flex-1 justify-center items-center">
+        <View className=" absolute top-4 w-[100vw] h-[80vh] rounded-2xl justify-start items-center py-5">
+          <Text className="text-white text-base font-semibold">
             Choose your interests
           </Text>
           <ScrollView
-            className='w-80 h-40 mt-4'
+            className="w-80 h-40 mt-4"
             contentContainerStyle={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'row',
-                flexWrap: 'wrap',
-                }}
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "row",
+              flexWrap: "wrap",
+            }}
           >
             {interests.map((interest) => (
               <TouchableOpacity
                 key={interest}
                 className={`${
                   selectedInterests.includes(interest)
-                    ? 'bg-[#3fe7f6]'
-                    : 'bg-[#E8E8E8]'
+                    ? "bg-[#3fe7f6]"
+                    : "bg-[#E8E8E8]"
                 } w-32 h-12 flex-row justify-center items-center rounded-full mt-4 mx-2`}
-                onPress={() => handleInterestSelection(interest)}
+                // onPress={() => handleInterestSelection(interest)}
+                onPress={() =>
+                  selectedInterests.includes(interest)
+                    ? handleInterestSelection(interest)
+                    : selectedInterests.length < 3
+                    ? handleInterestSelection(interest)
+                    : Toast.show({
+                        type: "error",
+                        text1: "Limit exceeded!! Select only 3..",
+                        visibilityTime: 3000,
+                        autoHide: true,
+                      })
+                }
               >
-                <Text className='text-base font-semibold'>{interest}</Text>
+                <Text className="text-base font-semibold">{interest}</Text>
               </TouchableOpacity>
             ))}
           </ScrollView>
           <TouchableOpacity
-            className='bg-[#22A6B3] w-80 h-12 flex-row justify-center items-center mt-4'
+            className="bg-[#22A6B3] w-80 h-12 flex-row justify-center items-center mt-4"
             onPress={handleInterestSubmit}
           >
-            <Text className='text-base font-semibold'>Save</Text>
+            <Text className="text-base font-semibold">Save</Text>
           </TouchableOpacity>
         </View>
       </View>
       <Toast config={ToastConfig} />
     </Modal>
-  )
-}
+  );
+};
 
-export default InterestPickerModal
+export default InterestPickerModal;
